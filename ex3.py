87498f082e30417b8b11759b48c2f0679800ff77
gwascak <gwascak@gmail.com>
2016-10-05 12:56:33 -0400

    Learning Python Ex 3.

---
+++ ex3.py
@@ -0,0 +1,35 @@
+# prints out the below text.
+print "I will now count my chickens:"
+
+# prints the word Hens and then does the math.
+print "Hens", 25.0 + 30.0 / 6.0
+# prints the word Roosters and then does the math.
+print "Roosters", 100.0 - 25.0 * 3.0 % 4
+
+# prints out the below text.
+print "Now I will count the eggs:"
+
+# does the math.
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# prints out the below text.
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# does the math and answers false or true on the equation.
+print 3.0 + 2.0 < 5.0 - 7.0
+
+# prints out the below text and does the math.
+print "Where is 3 + 2?", 3.0 + 2.0
+# prints out the below text and does the math.
+print "Where is 5 - 7?", 5.0 - 7.0
+
+# prints out the below text.
+print "Oh, that's why it's False."
+
+# prints out the below text.
+print "How about some more."
+
+# prints the below text and does the math for all of it.
+print "Is it greater?", 5.0 > 2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal", 5.0 <= -2.0
