75dfb63de08fc0af4ff44af9eae05448fbebc67f
gwascak <gwascak@gmail.com>
2016-10-06 13:03:09 -0400

    Learning Python Ex 12.

---
+++ ex12.py
@@ -0,0 +1,7 @@
+# Here we define 3 variables, request the user to input data based on the questions asked.
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("how much do you weight? ")
+
+print "So, you're %r old, %r tall and %r heavy." % (
+age, height, weight)
