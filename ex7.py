b87440986f49930fccca4f8747fede635aa5ee8c
gwascak <gwascak@gmail.com>
2016-10-06 08:18:52 -0400

    Learning Python Ex 7.

---
+++ ex7.py
@@ -0,0 +1,26 @@
+# Prints the next four lines.  The last line prints the character between the quotes 10 times.
+print "Mary had a little lamb."
+print "It's fleece was white as %s" % 'snow'
+print "And everywhere that Mary went."
+print "." * 10 # what'd that do?
+
+# We've defined 12 variables.
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Here we print out each of the variables from above.
+# The comma puts a space between the letters and keeps them on the same line.
+# If you remove the comma, it places the second line on a brand new line.
+# watch that comma at the end. try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
